6eab8d3a170a33b95a3330b3fb8a4ce8a7772e03
mitya5247 <mitya5247@yandex.ru>
2024-07-11 14:57:30 +0300

    feat: общение между сервисами по HTTP-запросам User

--- gateway/shareIt-gateway/src/main/java/ru/practicum/user/UserClient.java
+++ gateway/shareIt-gateway/src/main/java/ru/practicum/user/UserClient.java
@@ -1,41 +1,31 @@
 package ru.practicum.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.elasticsearch.RestClientBuilderCustomizer;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.boot.web.client.RootUriTemplateHandler;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.DefaultUriBuilderFactory;
-import ru.practicum.Constants;
-import ru.practicum.client.BaseClient;
 
-import java.lang.annotation.ElementType;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import ru.practicum.client.BaseClient;
 
 @Service
 public class UserClient extends BaseClient {
 
-    String uriPrefix;
-
     @Autowired
-    public UserClient(RestTemplateBuilder restTemplateBuilder, @Value(value = "${server-uri}")
-                      String uriPrefix) {
-        super(restTemplateBuilder.
-                uriTemplateHandler(new DefaultUriBuilderFactory(uriPrefix + Constants.PATH_USERS)).build());
-        this.uriPrefix = uriPrefix;
+    public UserClient(RestTemplateBuilder builder) {
+        super(builder.uriTemplateHandler(new DefaultUriBuilderFactory()).
+                requestFactory(HttpComponentsClientHttpRequestFactory::new).build());
     }
 
     @Override
     protected <T> ResponseEntity<Object> post(String path, T body) {
-        return super.post(uriPrefix, body);
+        return super.post(path, body);
     }
 
     @Override
-    protected <T> ResponseEntity<Object> patch(String path, long userId, T body) {
-        return super.patch(path, userId, body);
+    protected <T> ResponseEntity<Object> patch(String path, T body) {
+        return super.patch(path, body);
     }
 
     @Override
--- gateway/shareIt-gateway/src/main/java/ru/practicum/user/UserControllerGateway.java
+++ gateway/shareIt-gateway/src/main/java/ru/practicum/user/UserControllerGateway.java
@@ -8,41 +8,47 @@ import org.springframework.web.bind.annotation.*;
 import ru.practicum.Constants;
 import ru.practicum.user.dto.User;
 
-import javax.persistence.EntityNotFoundException;
 import javax.validation.Valid;
 
 @RestController
 @RequestMapping(path = Constants.PATH_USERS)
 public class UserControllerGateway {
+
     private final UserClient client;
 
+    private final String baseUri;
+
     @Autowired
-    public UserControllerGateway(UserClient client) {
+    public UserControllerGateway(UserClient client, @Value("${server-uri}") String baseUri) {
         this.client = client;
+        this.baseUri = baseUri + Constants.PATH_USERS;
     }
 
     @PostMapping
     public ResponseEntity<Object> create(@Valid @RequestBody User user) {
-        return client.post("http://localhost:8081/users", user);
+        return client.post(baseUri, user);
     }
 
     @PatchMapping(Constants.PATH_USER_ID)
-    public ResponseEntity<Object> update(@PathVariable Long userId, @RequestBody User user) throws EntityNotFoundException {
-        return client.patch(Constants.PATH_USERS, userId, user);
+    public ResponseEntity<Object> update(@PathVariable Long userId, @RequestBody User user) {
+        String uri = baseUri + "/" + userId;
+        return client.patch(uri, user);
     }
 
     @DeleteMapping(Constants.PATH_USER_ID)
-    public void delete(@PathVariable Long userId) throws EntityNotFoundException {
-        client.delete(Constants.PATH_USERS, userId);
+    public void delete(@PathVariable Long userId) {
+        String uri = baseUri + "/" + userId;
+        client.delete(uri, userId);
     }
 
     @GetMapping(Constants.PATH_USER_ID)
-    public ResponseEntity<Object> get(@PathVariable Long userId) throws EntityNotFoundException {
-        return client.get(Constants.PATH_USERS, userId);
+    public ResponseEntity<Object> get(@PathVariable Long userId) {
+        String uri = baseUri + "/" + userId;
+        return client.get(uri, userId);
     }
 
     @GetMapping
     public ResponseEntity<Object> getAll() {
-        return client.get(Constants.PATH_USERS);
+        return client.get(baseUri);
     }
 }
