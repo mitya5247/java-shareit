10e4b2d14a31dce834b01c9af1e0f28092ffc71f
mitya5247 <mitya5247@yandex.ru>
2024-07-13 18:48:08 +0300

    fix: поправил pom.xml в gateway

--- shareIt-gateway/src/main/java/ru/practicum/booking/BookingControllerGateway.java
+++ shareIt-gateway/src/main/java/ru/practicum/booking/BookingControllerGateway.java
@@ -4,6 +4,7 @@ import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.Constants;
